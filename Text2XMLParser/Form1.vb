b84d805f426718c8d87a6aa0b259beb5c827ca0f
Fernando Aguilar <feaguila@microsoft.com>
2019-03-15 17:51:59 -0700

    Upgrade to net472. Installed NuGet package

--- Text2XMLParser/Form1.vb
+++ Text2XMLParser/Form1.vb
@@ -1,12 +1,13 @@
-﻿Public Class Form1
+﻿
+Public Class Form1
 
     Private Sub btnCarga_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCarga.Click
         Dim opf As New OpenFileDialog
-        Dim obj As Text2XML
+        Dim obj As Text2Xml.Text2Xml
 
         Dim res As String
         If opf.ShowDialog() = Windows.Forms.DialogResult.OK Then
-            obj = New Text2XML()
+            obj = New Text2Xml.Text2Xml()
             res = obj.Parse(opf.FileName)
             System.IO.File.WriteAllText("salida.xml", res)
         End If
